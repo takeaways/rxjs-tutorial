162c8a1575a1c661fe98591c8106b6c80c2c5579
nomadGeonilJang <wkdrjsdlf2@gmail.com>
2020-10-15 22:51:02 +0900

    test1

--- index.js
+++ index.js
@@ -1,63 +1,80 @@
-/*
-    [         Stream         ]
-    --p3(1s)--------p2(1s)-------p1(1s)--> 시간
-                         = 포장 개봉 ( 5 초 ) 작업1
-                         = 상품 검사 ( 5 초 ) 작업2
-                         = 상품 사용 ( 5 초 ) 작업3
-    1. 초당 하나의 택배를 받겠다.
-*/
-const Rx = require('rxjs');
-const { mergeMap, take, map, tap, mergeAll } = require('rxjs/operators');
+/**
+ *
+ *  복잡한 택배 시스템
+ *  1000개의 택배가 1초에 한 번씩 배송이된다.
+ *  택배를 받으면 그 즉시 어래의 작업을 실행한다.
+ *
+ *  1. 상품 개봉 (3 초)
+ *  2. 상품 검사 (3 초)
+ *  3. 상품 사용 (3 초)
+ *
+ *  이때 택배 회사는 종업원이 3명 밖에 없기 때문에 위 작업은 최대 3명에 의해서 동시에 실행 될 수 있다.
+ *  즉, 동시에 4개 이상의 작업은 실행 될 수 없다.
+ *
+ *  각 택배들에 대해서 사품 사용까지 종료된 택배들을 10개씩 묶어서 공항으로 보낸다.
+ *
+ */
+
+const {
+    delay,
+    tap,
+    take,
+    concatAll,
+    concatMap,
+    map,
+    mergeAll,
+    reduce,
+    bufferCount,
+} = require('rxjs/operators');
+const { of, from, interval } = require('rxjs');
 
 function openBox(data) {
-    return new Promise((res, rej) => {
-        setTimeout(() => {
-            console.log(data, '상품개봉');
-            res(data);
-        }, 5000);
-    });
+    return of(data) //
+        .pipe(
+            delay(3000),
+            tap((data) => console.log(`${data}를 열었습니다.`))
+        );
 }
-
 function checkBox(data) {
-    return new Promise((res, rej) => {
-        setTimeout(() => {
-            console.log(data, '상품확인');
-            res(data);
-        }, 5000);
-    });
+    return of(data) //
+        .pipe(
+            delay(3000),
+            tap((data) => console.log(`${data}를 확인했습니다..`))
+        );
 }
-
 function useProduct(data) {
-    return new Promise((res, rej) => {
-        setTimeout(() => {
-            console.log(data, '상품사용');
-            res(data);
-        }, 5000);
-    });
+    return of(data) //
+        .pipe(
+            delay(3000),
+            tap((data) => console.log(`${data}를 사용했습니다.`))
+        );
 }
 
-async function useTask(data) {
-    await openBox(data);
-    await checkBox(data);
-    await useProduct(data);
+const deliveries = interval(1000).pipe(take(1000));
+
+function doTask(delivery) {
+    const tasks = from([
+        openBox(delivery),
+        checkBox(delivery),
+        useProduct(delivery),
+    ]);
+    return tasks.pipe(
+        concatAll(),
+        reduce((a, c) => {
+            return delivery;
+        })
+    );
 }
 
-const stream = Rx.interval(1000).pipe(
-    take(3),
-    map((data) => `택배${data + 1}`)
-);
-stream // 다른 옵져버블을 리턴 받아야 한다. 옵져버블을 리턴 받아야 한다.
-    .pipe(mergeMap((data) => Rx.from(useTask(data))))
-    .subscribe();
+function sendToAirport(data) {
+    console.log('----->', data);
+}
 
-// merge (all)
-const stream1 = Rx.interval(1000).pipe(take(3), tap(console.log));
-const stream2 = Rx.interval(1000).pipe(take(3), tap(console.log));
-const stream3 = Rx.interval(1000).pipe(take(3), tap(console.log));
-const stream4 = Rx.interval(1000).pipe(take(3), tap(console.log));
-const stream5 = Rx.interval(1000).pipe(take(3), tap(console.log));
-const stream10 = Rx.of(stream1, stream2, stream3, stream4, stream5);
-
-stream10 //
-    .pipe(mergeAll(2)) //묶음 단위
+deliveries
+    .pipe(
+        map((delivery) => doTask(delivery)),
+        mergeAll(3),
+        bufferCount(10), //앞 단계에서 방출된 값을 누적
+        tap((ten) => sendToAirport(ten))
+    )
     .subscribe();
